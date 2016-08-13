a803e65ad219d6b541010f0faf974aa4e5476c49
Roy Wei <roywjy@gmail.com>
2016-08-13 15:53:55 +0800

    testing template and add font-awesome

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'reviews/create'
 
-  root to: 'pages#index'
+  root to: 'restaurants#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
