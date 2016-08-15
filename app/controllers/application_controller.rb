024ade0c95e9ae475b55012d493228874a7a5a74
Roy Wei <roywjy@gmail.com>
2016-08-15 23:32:28 +0800

    add more columns in registration pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit( :email,
+                          :password,
+                          :password_confirmation,
+                          :username,
+                          :first_name,
+                          :last_name)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |user_params|
+      user_params.permit( :email,
+                          :password,
+                          :password_confirmation,
+                          :username,
+                          :first_name,
+                          :last_name)
+    end
+  end
 end
+
