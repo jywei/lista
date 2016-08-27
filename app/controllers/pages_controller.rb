229c8b12419157fed722c5364f3421f8d29d9346
Roy Wei <roywjy@gmail.com>
2016-08-27 15:50:43 +0800

    try page routing problems and reset the controller and model

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,31 @@
 class PagesController < ApplicationController
+  before_action :set_page, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :admin?, only: [:edit, :update]
+
+  # GET /pages
+  # GET /pages.json
   def index
+    @page = Page.find(1)
   end
 
   def about
+    @restaurant_1 = Restaurant.find(1)
+    @restaurant_2 = Restaurant.find(2)
+    @restaurant_3 = Restaurant.find(3)
+    @page = Page.find(2)
   end
 
   def contact
+    @page = Page.find(3)
   end
 
+  # GET /pages/1
+  # GET /pages/1.json
   def show
-    #   @restaurant_1 = Restaurant.find(1)
-    # @restaurant_2 = Restaurant.find(2)
-    # @restaurant_3 = Restaurant.find(3)
+    @restaurant_1 = Restaurant.find(1)
+    @restaurant_2 = Restaurant.find(2)
+    @restaurant_3 = Restaurant.find(3)
   end
 
   # GET /pages/new
@@ -42,6 +56,8 @@ class PagesController < ApplicationController
     end
   end
 
+
+
   private
 
   def admin?
@@ -51,13 +67,14 @@ class PagesController < ApplicationController
     end
   end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_page
-    @page = Page.find(params[:id])
-  end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def page_params
-    params.require(:page).permit(:string_1, :string_2, :string_3, :string_4, :string_5, :string_6, :string_7, :string_8, :string_9, :string_10, :string_11, :string_12, :string_13, :string_14, :string_15, :string_16, :text_1, :text_2, :text_3, :text_4, :text_5, :text_6, :text_7, :text_8, :text_9, :text_10, :page_name, :integer_1, :integer_2, :integer_3, :integer_4, :image_1, :image_2, :image_3, :image_4)
+    # Use callbacks to share common setup or constraints between actions.
+    def set_page
+      @page = Page.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def page_params
+      params.require(:page).permit(:string_1, :string_2, :string_3, :string_4, :string_5, :string_6, :string_7, :string_8, :string_9, :string_10, :string_11, :string_12, :string_13, :string_14, :string_15, :string_16, :text_1, :text_2, :text_3, :text_4, :text_5, :text_6, :text_7, :text_8, :text_9, :text_10, :page_name, :integer_1, :integer_2, :integer_3, :integer_4, :image_1, :image_2, :image_3, :image_4)
+    end
   end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,15 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-
-  get 'reviews/new'
-
-  get 'reviews/create'
-
-  # resources 'pages'
-
-  resources 'restaurants' do
-    resources 'reviews', except: [:show, :index]
-    collection do
-      get 'search_restaurant'
-    end
-  end
-
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
-  get 'pages/index' => 'pages#index'
-
-  root to: 'restaurants#index'
 
   devise_for :users
+  get 'pages/about', controller: 'pages', action: 'about'
+  get 'pages/contact', controller: 'pages', action: 'contact'
+  resources :pages
+  resources :restaurants do
+    resources :reviews, except: [:show, :index]
+    collection do
+      get 'search'
+    end
+  end
+  root to: 'restaurants#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
